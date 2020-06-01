cca6b7355a72ad7aa7557f0eaa8964d8633c862e
daniilty <adef96@mail.ru>
2020-06-01 23:01:30 +0300

    initial commit

---
+++ pochta.rb
@@ -0,0 +1,28 @@
+require 'httparty'
+require 'nokogiri'
+
+class Info
+
+  attr_accessor :page
+  
+  def initialize(addr)
+    if addr.nil?
+      puts "Please gimme your track number"
+      return
+    else
+      doc = HTTParty.get("https://gdeposylka.ru/courier/china-ems/tracking/" + addr)
+      @page = Nokogiri::HTML(doc)
+    end
+   end
+   def prin()
+     @page.xpath('//ul[@class = "checkpoints"]/li').each do |el|
+      puts el.text
+     end
+   end
+
+end
+#print("Enter the tracking number: ")
+#inf = gets
+inst = Info.new("LC118255675CN")
+#inst = Info.new(inf.slice(..-2))
+inst.prin
